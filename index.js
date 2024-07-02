b9c64e3fe32ee735b7fc35dda7ad71ec5ede15da
duccanhole <duccanhole@gmail.com>
2024-07-02 22:41:00 +0700

    open video, use svg icon instead symbol

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ const containerTemplate =
     <button
       id="lightbox-action-download"
       class="lightbox-action-btn"
-    ></button>
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+        <path d="M480-322.87 268.52-534.35l63.89-65.41L434.5-497.44v-310.69h91v310.69l102.09-102.32 63.89 65.41L480-322.87Zm-237.13 171q-37.78 0-64.39-26.61t-26.61-64.39v-120h91v120h474.26v-120h91v120q0 37.78-26.61 64.39t-64.39 26.61H242.87Z"/>
+      </svg>
+    </button>
     <button
       id="lightbox-action-rotate"
       class="lightbox-action-btn"
+      style="display: none"
     ></button>
-    <button id="lightbox-action-zoom" class="lightbox-action-btn"></button>
-    <button id="lightbox-action-reset" class="lightbox-action-btn"></button>
-    <button id="lightbox-action-gallery" class="lightbox-action-btn"></button>
-    <button id="lightbox-action-close" class="lightbox-action-btn"></button>
+    <button id="lightbox-action-zoom" class="lightbox-action-btn" style="display: none"></button>
+    <button id="lightbox-action-reset" class="lightbox-action-btn" style="display: none"></button>
+    <button id="lightbox-action-gallery" class="lightbox-action-btn">
+      <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+        <path d="M236.44-152.59q-34.46 0-59.16-24.69-24.69-24.7-24.69-59.16 0-34.47 24.69-59.02Q201.98-320 236.44-320q34.47 0 59.02 24.54Q320-270.91 320-236.44q0 34.46-24.54 59.16-24.55 24.69-59.02 24.69Zm243.59 0q-34.46 0-59.04-24.69-24.58-24.7-24.58-59.16 0-34.47 24.55-59.02Q445.5-320 479.97-320q34.46 0 59.04 24.54 24.58 24.55 24.58 59.02 0 34.46-24.55 59.16-24.54 24.69-59.01 24.69Zm243.53 0q-34.47 0-59.02-24.69Q640-201.98 640-236.44q0-34.47 24.54-59.02Q689.09-320 723.56-320q34.46 0 59.16 24.54 24.69 24.55 24.69 59.02 0 34.46-24.69 59.16-24.7 24.69-59.16 24.69ZM236.44-396.41q-34.46 0-59.16-24.55-24.69-24.54-24.69-59.01 0-34.46 24.69-59.04 24.7-24.58 59.16-24.58 34.47 0 59.02 24.55Q320-514.5 320-480.03q0 34.46-24.54 59.04-24.55 24.58-59.02 24.58Zm243.59 0q-34.46 0-59.04-24.55-24.58-24.54-24.58-59.01 0-34.46 24.55-59.04 24.54-24.58 59.01-24.58 34.46 0 59.04 24.55 24.58 24.54 24.58 59.01 0 34.46-24.55 59.04-24.54 24.58-59.01 24.58Zm243.53 0q-34.47 0-59.02-24.55Q640-445.5 640-479.97q0-34.46 24.54-59.04 24.55-24.58 59.02-24.58 34.46 0 59.16 24.55 24.69 24.54 24.69 59.01 0 34.46-24.69 59.04-24.7 24.58-59.16 24.58ZM236.44-640q-34.46 0-59.16-24.54-24.69-24.55-24.69-59.02 0-34.46 24.69-59.16 24.7-24.69 59.16-24.69 34.47 0 59.02 24.69Q320-758.02 320-723.56q0 34.47-24.54 59.02Q270.91-640 236.44-640Zm243.59 0q-34.46 0-59.04-24.54-24.58-24.55-24.58-59.02 0-34.46 24.55-59.16 24.54-24.69 59.01-24.69 34.46 0 59.04 24.69 24.58 24.7 24.58 59.16 0 34.47-24.55 59.02Q514.5-640 480.03-640Zm243.53 0q-34.47 0-59.02-24.54Q640-689.09 640-723.56q0-34.46 24.54-59.16 24.55-24.69 59.02-24.69 34.46 0 59.16 24.69 24.69 24.7 24.69 59.16 0 34.47-24.69 59.02Q758.02-640 723.56-640Z"/>
+      </svg>
+    </button>
+    <button id="lightbox-action-close" class="lightbox-action-btn">
+      <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+        <path d="M256-192.35 192.35-256l224-224-224-224L256-767.65l224 224 224-224L767.65-704l-224 224 224 224L704-192.35l-224-224-224 224Z"/>
+      </svg>
+    </button>
   </div>
   <div id="lightbox-content" class="lightbox-content">
 <!--</div>-->
@@ -152,6 +165,8 @@ class Lightbox {
   }
 
   #onClose() {
+    const contentContainer = document.getElementById('lightbox-content')
+    contentContainer.replaceChildren()
     this.#modalContainer.style.display = 'none'
   }
 
@@ -184,9 +199,9 @@ class Lightbox {
     const { type } = data
     let shouldOpen = true
     const contentContainer = document.getElementById('lightbox-content')
+    contentContainer.replaceChildren()
     switch (type) {
       case "image":
-        contentContainer.replaceChildren()
         const imageContainer = document.createElement('div')
         imageContainer.id = "lightbox-image-container"
         imageContainer.setAttribute('data-src', data.url)
@@ -197,7 +212,25 @@ class Lightbox {
         this.#initImageAction()
         this.#onReset()
         break;
+      case "video":
+        const videoContainer = document.createElement('div')
+        videoContainer.id = "lightbox-video-container"
+        videoContainer.className = 'lightbox-video-container'
 
+        const video = document.createElement('video')
+        video.className = 'lightbox-video'
+        video.controls = true
+        video.style.width = '100%'
+
+        for(const s of data.sources) {
+          const source = document.createElement('source')
+          source.src = s.src 
+          source.type = s.type
+          video.appendChild(source)
+        }
+        videoContainer.appendChild(video)
+        contentContainer.appendChild(videoContainer)
+        break;
       default:
         shouldOpen = false
         break;
