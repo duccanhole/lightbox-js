e86ea92c823a09b3971452d44041139c8e16469f
duccanhole <duccanhole@gmail.com>
2024-07-03 21:39:35 +0700

    add gallery action

--- index.js
+++ index.js
@@ -14,9 +14,21 @@ const containerTemplate =
       id="lightbox-action-rotate"
       class="lightbox-action-btn"
       style="display: none"
-    ></button>
-    <button id="lightbox-action-zoom" class="lightbox-action-btn" style="display: none"></button>
-    <button id="lightbox-action-reset" class="lightbox-action-btn" style="display: none"></button>
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+        <path d="M522-69.48v-93q33.28-4.76 65.18-17.52 31.91-12.76 60.67-33.52l64.13 65.65q-42.24 33.44-90.03 53.41-47.8 19.98-99.95 24.98Zm-80 0Q300.65-87.72 207.26-191.49q-93.39-103.77-93.39-243.88 0-75.96 28.5-142.77 28.5-66.82 77.36-116.63 48.86-49.82 114.72-79.03 65.85-29.22 141.57-29.22h5.05l-57.22-57.46 63.65-65.17L655.15-758 487.5-590.35 423.85-654l58.02-58.02h-6.57q-113.89 0-192.16 81.5t-78.27 195.15q0 102.57 67.04 179.99 67.05 77.42 170.09 92.9v93Zm327.98-136.15-65.89-63.89q20.76-28.76 33.52-60.55 12.76-31.78 17.76-65.3h92.76q-5 51.67-24.98 99.23-19.98 47.55-53.17 90.51Zm78.15-269.74h-92.76q-5.24-33.52-18-65.54t-33.52-60.79l66.13-63.17q33.43 40.44 53.03 88.87 19.6 48.43 25.12 100.63Z"/>
+      </svg>
+    </button>
+    <button id="lightbox-action-zoom" class="lightbox-action-btn" style="display: none">
+      <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+        <path d="M783.52-110.91 529.85-364.59q-29.76 23.05-68.64 36.57-38.88 13.52-83.12 13.52-111.16 0-188.33-77.17-77.17-77.18-77.17-188.33t77.17-188.33q77.17-77.17 188.33-77.17 111.15 0 188.32 77.17 77.18 77.18 77.18 188.33 0 44.48-13.52 83.12-13.53 38.64-36.57 68.16l253.91 254.15-63.89 63.66ZM378.09-405.5q72.84 0 123.67-50.83 50.83-50.82 50.83-123.67t-50.83-123.67q-50.83-50.83-123.67-50.83-72.85 0-123.68 50.83-50.82 50.82-50.82 123.67t50.82 123.67q50.83 50.83 123.68 50.83Zm-40-55.93V-540h-78.57v-80h78.57v-78.57h80V-620h78.56v80h-78.56v78.57h-80Z"/>
+      </svg>
+    </button>
+    <button id="lightbox-action-reset" class="lightbox-action-btn" style="display: none">
+      <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+        <path d="M783.52-110.91 529.85-364.59q-29.76 23.05-68.64 36.57-38.88 13.52-83.12 13.52-111.16 0-188.33-77.17-77.17-77.18-77.17-188.33t77.17-188.33q77.17-77.17 188.33-77.17 111.15 0 188.32 77.17 77.18 77.18 77.18 188.33 0 44.48-13.52 83.12-13.53 38.64-36.57 68.16l253.91 254.15-63.89 63.66ZM378.09-405.5q72.84 0 123.67-50.83 50.83-50.82 50.83-123.67t-50.83-123.67q-50.83-50.83-123.67-50.83-72.85 0-123.68 50.83-50.82 50.82-50.82 123.67t50.82 123.67q50.83 50.83 123.68 50.83Zm-100-134.5v-80h200v80h-200Z"/>
+      </svg>
+    </button>
     <button id="lightbox-action-gallery" class="lightbox-action-btn">
       <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
         <path d="M236.44-152.59q-34.46 0-59.16-24.69-24.69-24.7-24.69-59.16 0-34.47 24.69-59.02Q201.98-320 236.44-320q34.47 0 59.02 24.54Q320-270.91 320-236.44q0 34.46-24.54 59.16-24.55 24.69-59.02 24.69Zm243.59 0q-34.46 0-59.04-24.69-24.58-24.7-24.58-59.16 0-34.47 24.55-59.02Q445.5-320 479.97-320q34.46 0 59.04 24.54 24.58 24.55 24.58 59.02 0 34.46-24.55 59.16-24.54 24.69-59.01 24.69Zm243.53 0q-34.47 0-59.02-24.69Q640-201.98 640-236.44q0-34.47 24.54-59.02Q689.09-320 723.56-320q34.46 0 59.16 24.54 24.69 24.55 24.69 59.02 0 34.46-24.69 59.16-24.7 24.69-59.16 24.69ZM236.44-396.41q-34.46 0-59.16-24.55-24.69-24.54-24.69-59.01 0-34.46 24.69-59.04 24.7-24.58 59.16-24.58 34.47 0 59.02 24.55Q320-514.5 320-480.03q0 34.46-24.54 59.04-24.55 24.58-59.02 24.58Zm243.59 0q-34.46 0-59.04-24.55-24.58-24.54-24.58-59.01 0-34.46 24.55-59.04 24.54-24.58 59.01-24.58 34.46 0 59.04 24.55 24.58 24.54 24.58 59.01 0 34.46-24.55 59.04-24.54 24.58-59.01 24.58Zm243.53 0q-34.47 0-59.02-24.55Q640-445.5 640-479.97q0-34.46 24.54-59.04 24.55-24.58 59.02-24.58 34.46 0 59.16 24.55 24.69 24.54 24.69 59.01 0 34.46-24.69 59.04-24.7 24.58-59.16 24.58ZM236.44-640q-34.46 0-59.16-24.54-24.69-24.55-24.69-59.02 0-34.46 24.69-59.16 24.7-24.69 59.16-24.69 34.47 0 59.02 24.69Q320-758.02 320-723.56q0 34.47-24.54 59.02Q270.91-640 236.44-640Zm243.59 0q-34.46 0-59.04-24.54-24.58-24.55-24.58-59.02 0-34.46 24.55-59.16 24.54-24.69 59.01-24.69 34.46 0 59.04 24.69 24.58 24.7 24.58 59.16 0 34.47-24.55 59.02Q514.5-640 480.03-640Zm243.53 0q-34.47 0-59.02-24.54Q640-689.09 640-723.56q0-34.46 24.54-59.16 24.55-24.69 59.02-24.69 34.46 0 59.16 24.69 24.69 24.7 24.69 59.16 0 34.47-24.69 59.02Q758.02-640 723.56-640Z"/>
@@ -28,7 +40,19 @@ const containerTemplate =
       </svg>
     </button>
   </div>
-  <div id="lightbox-content" class="lightbox-content">
+  <div id="lightbox-content" class="lightbox-content"></div>
+  <div id="lighbox-bottom" class="lightbox-bottom">
+    <div class="lightbox-gallery-action">
+      <button id="lightbox-gallery-btn" class="lightbox-gallery-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
+          <path d="M236.44-152.59q-34.46 0-59.16-24.69-24.69-24.7-24.69-59.16 0-34.47 24.69-59.02Q201.98-320 236.44-320q34.47 0 59.02 24.54Q320-270.91 320-236.44q0 34.46-24.54 59.16-24.55 24.69-59.02 24.69Zm243.59 0q-34.46 0-59.04-24.69-24.58-24.7-24.58-59.16 0-34.47 24.55-59.02Q445.5-320 479.97-320q34.46 0 59.04 24.54 24.58 24.55 24.58 59.02 0 34.46-24.55 59.16-24.54 24.69-59.01 24.69Zm243.53 0q-34.47 0-59.02-24.69Q640-201.98 640-236.44q0-34.47 24.54-59.02Q689.09-320 723.56-320q34.46 0 59.16 24.54 24.69 24.55 24.69 59.02 0 34.46-24.69 59.16-24.7 24.69-59.16 24.69ZM236.44-396.41q-34.46 0-59.16-24.55-24.69-24.54-24.69-59.01 0-34.46 24.69-59.04 24.7-24.58 59.16-24.58 34.47 0 59.02 24.55Q320-514.5 320-480.03q0 34.46-24.54 59.04-24.55 24.58-59.02 24.58Zm243.59 0q-34.46 0-59.04-24.55-24.58-24.54-24.58-59.01 0-34.46 24.55-59.04 24.54-24.58 59.01-24.58 34.46 0 59.04 24.55 24.58 24.54 24.58 59.01 0 34.46-24.55 59.04-24.54 24.58-59.01 24.58Zm243.53 0q-34.47 0-59.02-24.55Q640-445.5 640-479.97q0-34.46 24.54-59.04 24.55-24.58 59.02-24.58 34.46 0 59.16 24.55 24.69 24.54 24.69 59.01 0 34.46-24.69 59.04-24.7 24.58-59.16 24.58ZM236.44-640q-34.46 0-59.16-24.54-24.69-24.55-24.69-59.02 0-34.46 24.69-59.16 24.7-24.69 59.16-24.69 34.47 0 59.02 24.69Q320-758.02 320-723.56q0 34.47-24.54 59.02Q270.91-640 236.44-640Zm243.59 0q-34.46 0-59.04-24.54-24.58-24.55-24.58-59.02 0-34.46 24.55-59.16 24.54-24.69 59.01-24.69 34.46 0 59.04 24.69 24.58 24.7 24.58 59.16 0 34.47-24.55 59.02Q514.5-640 480.03-640Zm243.53 0q-34.47 0-59.02-24.54Q640-689.09 640-723.56q0-34.46 24.54-59.16 24.55-24.69 59.02-24.69 34.46 0 59.16 24.69 24.69 24.7 24.69 59.16 0 34.47-24.69 59.02Q758.02-640 723.56-640Z"/>
+        </svg>
+      </button>
+    </div>
+    <div class="lightbox-gallery-container">
+      <div class="lightbox-gallery-item"></div>
+    </div>
+  </div>
 <!--</div>-->
 `
 // todos: lazy loading image
@@ -38,6 +62,7 @@ class Lightbox {
   #imageContainer
 
   // Action
+  #downloadBtn
   #closeBtn
   #rotateBtn
   #zoomBtn
@@ -53,9 +78,12 @@ class Lightbox {
   #endX = 0
   #endY = 0
   #isPanning = false
+
+  #downloadUrl = null
   constructor() {
     this.#initContainer()
     this.#initAction()
+    this.#initGalleryAction()
   }
   #initContainer() {
     let el = document.getElementById('lightbox')
@@ -74,23 +102,31 @@ class Lightbox {
   #initAction() {
     this.#closeBtn = document.getElementById('lightbox-action-close')
     this.#closeBtn.onclick = () => this.#onClose()
+
+    this.#downloadBtn = document.getElementById('lightbox-action-download')
+    this.#downloadBtn.onclick = () => {
+      window.open(this.#downloadUrl)
+    }
   }
 
   #initImageAction() {
     this.#imageContainer = document.getElementById('lightbox-image-container')
     this.#rotateBtn = document.getElementById('lightbox-action-rotate')
+    this.#rotateBtn.style.display = "block"
     this.#rotateBtn.onclick = () => {
       this.#degRotate += 90
       if (this.#degRotate > 360) this.#degRotate = 90
       this.#onRotate(this.#degRotate)
     }
     this.#zoomBtn = document.getElementById('lightbox-action-zoom')
+    this.#zoomBtn.style.display = "block"
     this.#zoomBtn.onclick = () => {
       this.#zoomSize ++;
       if (this.#zoomSize > 5) return
       this.#onZoom(this.#zoomSize)
     }
     this.#resetBtn = document.getElementById('lightbox-action-reset')
+    this.#resetBtn.style.display = "block"
     this.#resetBtn.onclick = () => {
       this.#endX = 0
       this.#endY = 0
@@ -164,6 +200,20 @@ class Lightbox {
     }
   }
 
+  #initGalleryAction(){
+    const topAction = document.getElementById('lightbox-action-gallery')
+    const bottomAction = document.getElementById('lightbox-gallery-btn')
+    const galleryContainer = document.getElementById('lighbox-bottom')
+
+    topAction.onclick = () => {
+      galleryContainer.style.display = 'block'
+    }
+
+    bottomAction.onclick = () => {
+      galleryContainer.style.display = 'none'
+    }
+  }
+
   #onClose() {
     const contentContainer = document.getElementById('lightbox-content')
     contentContainer.replaceChildren()
@@ -196,7 +246,7 @@ class Lightbox {
   }
 
   open(data) {
-    const { type } = data
+    const { type, downloadUrl } = data
     let shouldOpen = true
     const contentContainer = document.getElementById('lightbox-content')
     contentContainer.replaceChildren()
@@ -204,9 +254,9 @@ class Lightbox {
       case "image":
         const imageContainer = document.createElement('div')
         imageContainer.id = "lightbox-image-container"
-        imageContainer.setAttribute('data-src', data.url)
+        imageContainer.setAttribute('data-src', data.src)
         imageContainer.className = 'lightbox-image-container'
-        imageContainer.style['background-image'] = `url(${data.url})`
+        imageContainer.style['background-image'] = `url(${data.src})`
         contentContainer.appendChild(imageContainer)
 
         this.#initImageAction()
@@ -236,20 +286,17 @@ class Lightbox {
         break;
     }
     if (shouldOpen) {
+      this.#downloadUrl = downloadUrl
       this.#modalContainer.style.display = "block"
     }
     else {
       console.error("Type is not valid");
     }
-    // const content = document.getElementById("lightbox-content")
-    // const contentContainer = content.querySelector('div')
-    // contentContainer.setAttribute('data-src', url)
-    // contentContainer.style['background-image'] = `url(${url})`
-    // this.#onReset()
-    // this.#modalContainer.style.display = "block"
-    // this.#modalContainer.onclick = this.close
-    // document.body.appendChild(this.#modalContainer)
   }
+
+  setGallery(list){}
+
+  openAt(){}
 }
 
 export default Lightbox
